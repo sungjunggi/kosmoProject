27763ab97b565f2d30e334b83b8ec23a4358783f
kim-hw <76235990+kim-hw@users.noreply.github.com>
2022-11-24 17:11:38 +0900

    1

---
+++ KimHW/src/main/java/com/kimhw/KimHwApplication.java
@@ -0,0 +1,13 @@
+package com.kimhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimHwApplication.class, args);
+	}
+
+}
