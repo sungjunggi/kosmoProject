ece31d817b27b6393a580af22d6b14ddf7bb415c
kim-hw <76235990+kim-hw@users.noreply.github.com>
2022-11-24 18:16:55 +0900

    hw
    This reverts commit 0ff3ffda790e4a8d564a4c2f2cddf537a49dd88b.

---
+++ KimHyeonWooFinal/src/main/java/com/kimhw/KimHyeonWooFinalApplication.java
@@ -0,0 +1,13 @@
+package com.kimhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimHyeonWooFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimHyeonWooFinalApplication.class, args);
+	}
+
+}
