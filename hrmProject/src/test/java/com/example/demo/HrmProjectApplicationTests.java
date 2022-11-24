0e1e882f6522e58d8985db507b8bc98b41289418
kim-hw <76235990+kim-hw@users.noreply.github.com>
2022-11-24 17:30:05 +0900

    fin

--- hrmProject/src/test/java/com/example/demo/HrmProjectApplicationTests.java
+++ hrmProject/src/test/java/com/example/demo/HrmProjectApplicationTests.java
@@ -1,18 +1,26 @@
+<<<<<<< HEAD
 <<<<<<<< HEAD:KimHW/src/test/java/com/kimhw/KimHwApplicationTests.java
 package com.kimhw;
 ========
 package com.example.demo;
 >>>>>>>> 4ca73b4d5f28da2b6b6f8f5b6a60bb29fa612ece:hrmProject/src/test/java/com/example/demo/HrmProjectApplicationTests.java
+=======
+package com.example.demo;
+>>>>>>> 4ca73b4d5f28da2b6b6f8f5b6a60bb29fa612ece
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+<<<<<<< HEAD
 <<<<<<<< HEAD:KimHW/src/test/java/com/kimhw/KimHwApplicationTests.java
 class KimHwApplicationTests {
 ========
 class HrmProjectApplicationTests {
 >>>>>>>> 4ca73b4d5f28da2b6b6f8f5b6a60bb29fa612ece:hrmProject/src/test/java/com/example/demo/HrmProjectApplicationTests.java
+=======
+class HrmProjectApplicationTests {
+>>>>>>> 4ca73b4d5f28da2b6b6f8f5b6a60bb29fa612ece
 
 	@Test
 	void contextLoads() {
