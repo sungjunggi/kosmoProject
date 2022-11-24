393538d7615dce4126f823a5627a0704e69a8fb6
2emma <116706945+2emma@users.noreply.github.com>
2022-11-24 18:36:05 +0900

    mijin

---
+++ hrmProjectMijin/src/test/java/com/hrmProject/HrmProjectMijinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmProjectMijinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
