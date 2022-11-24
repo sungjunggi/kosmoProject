8cd3c16d2d31e8fb99d5c3afdd912fc756c9db1b
kim-hw <76235990+kim-hw@users.noreply.github.com>
2022-11-24 17:04:18 +0900

    Create FProjectApplication.java

---
+++ FProject/src/main/java/com/project/FProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FProjectApplication.class, args);
+	}
+
+}
