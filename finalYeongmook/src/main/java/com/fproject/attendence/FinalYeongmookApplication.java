acc2dd35226b65914e168faef80fab22011e96da
Emotion4Day7 <117630689+Emotion4Day7@users.noreply.github.com>
2022-11-29 16:42:30 +0900

    yeongmook

--- finalYeongmook/src/main/java/com/fproject/attendence/FinalYeongmookApplication.java
+++ finalYeongmook/src/main/java/com/fproject/attendence/FinalYeongmookApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.fproject.attendence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
