86545100a14df9834c97c98063f27bfb546f3e8b
kim-hw <76235990+kim-hw@users.noreply.github.com>
2022-12-14 12:12:30 +0900

    hw

--- finalHyeonWoo/src/main/java/com/kimhw/dto/Criteria.java
+++ finalHyeonWoo/src/main/java/com/kimhw/dto/Criteria.java
@@ -15,6 +15,22 @@ public class Criteria {
 	private int num;
 	private String type;
 	private String keyword;
+	private String communication_end;
+	private String communication_start;
+	private String communication_search;
+	//////
+	private int communication_num;
+	private String communication_title;
+	private String communication_date;
+	private String communication_assign;
+	private String communication_state;
+	private String employee_name;
+	private String communication_image;
+	private String communication_content;
+	private String category_title;
+	private int file_num;
+	private String bookmark_id;
+
 	
 	//생성자로 무조건 실행된다 1번은
 	//기본 페이지를 1페이지에 10개씩 보여준다는 의미
@@ -31,6 +47,7 @@ public class Criteria {
 	//매개변수로 들어오는 값을 이용하여 조정할 수 있다.
 	public Criteria(int pageNum, int amount) {
 		this.pageNum=pageNum;
+		System.out.println(pageNum+"페잉지잊잊이잽잊뱅ㅈ배잊뱅ㅂ");
 		this.amount=amount;
 	}
 	
