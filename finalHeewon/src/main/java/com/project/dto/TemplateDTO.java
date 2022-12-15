6edd0346c12bfda2dcf8ff48edf37a7da5935a55
heoheewon <117630664+heoheewon@users.noreply.github.com>
2022-12-15 10:09:20 +0900

    heewon

--- finalHeewon/src/main/java/com/project/dto/TemplateDTO.java
+++ finalHeewon/src/main/java/com/project/dto/TemplateDTO.java
@@ -8,10 +8,15 @@ import lombok.Setter;
 public class TemplateDTO {
 	private int gettemplate_num;
 	private String gettemplate_title;
-	private String gettemplate_question;
-	private String gettemplate_explanation;
-	private String gettemplate_option;
 	private String gettemplate_image;
 	private String gettemplate_date;
 	private String gettemplate_writer;
+	private String gettemplate_question1;
+	private String gettemplate_question2;
+	private String gettemplate_question3;
+	private String gettemplate_question4;
+	private String gettemplate_question5;
+	private String gettemplate_question6;
+	private String gettemplate_question7;
+
 }
