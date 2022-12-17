2a47f9e9760e968332ee071126903cda354b598e
heoheewon <117630664+heoheewon@users.noreply.github.com>
2022-12-17 21:40:50 +0900

    hee

--- finalHrmWar2/src/main/java/com/hrm/finalpj/report/dto/ReportDTO.java
+++ finalHrmWar2/src/main/java/com/hrm/finalpj/report/dto/ReportDTO.java
@@ -7,18 +7,15 @@ import lombok.Setter;
 @Setter
 public class ReportDTO {
 
-	private int template_board_num;
-	private int gettemplate_num;
 	private int employee_num;
-	private String gettemplate_title;
 	private String employee_name;
+	private int gettemplate_num;
+	private String gettemplate_title;
 	private int getfile_num;
-	private String template_board_date;
 	private String getfile_old_name;
+	private int template_board_num;
+	private String template_board_date;
 	private String gettemplate_image;
-	private String getcomment_content;
-	private String count;
-	private String sign_status;
 	private String template_board_answer1;
 	private String template_board_answer2;
 	private String template_board_answer3;
@@ -26,5 +23,8 @@ public class ReportDTO {
 	private String template_board_answer5;
 	private String template_board_answer6;
 	private String template_board_answer7;
-	
+	private String getcomment_content;
+	private String count;
+	private String template_board_sign_status;
+	private String template_board_sign_name;
 }
