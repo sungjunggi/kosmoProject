d40093f2d239ae413ebf3ecb844a5144f675b3b1
junjun9292 <117630749+junjun9292@users.noreply.github.com>
2022-12-29 22:02:40 +0900

    jy

--- finalHrmWar2/src/main/java/com/hrm/finalpj/dto/AttendanceDto2.java
+++ finalHrmWar2/src/main/java/com/hrm/finalpj/dto/AttendanceDto2.java
@@ -5,8 +5,33 @@ import lombok.Data;
 @Data
 public class AttendanceDto2 {
 	
+	private int attendance_num;
 	private String attendance_workstart;
 	private String attendance_workend;
+	
+	private int calendar_num;
+	private String calendar_date;
+	
+	private int department_num;
+	private String department_name;
+	private int position_num;
+	
+	private int employee_num;
+	private String employee_phone;
+	private String employee_type;
+	private String employee_date;
+	private String employee_quitdate;
+	private String employee_picture;
+	private String employee_gender;
+	private int id;
+	private String employee_name;
+	private String employee_id;
+	private String employee_password;
+	private String employee_address;
+	private String employee_email;
+	
+	private int job_num;
+	private String job_name;
 	
 	private int schedule_num;
 	private String schedule_name;
@@ -18,13 +43,19 @@ public class AttendanceDto2 {
 	
 	private int type_num;
 	private String type_category;
-
-	private int employee_num;
-	private String employee_name;
 	
-	private String job_num;
-	private String job_name;
+	private double average;
+	private int scheduleWorkCount;
+	private int todayWorkCount;
+	private int beforeWorkCount;
+	private int notWorkCount;
+	private int lateWorkCount;
+	private int leftEarlierCount;
+	private int businessTripCount;
+	private int vacationCount;
+	private int educationCount;
+	private int etcCount;
+	private int noScheduleCount;
+	private int defaultCount;
 	
-	private String attRate1;
-	private String attRate2;
 }
